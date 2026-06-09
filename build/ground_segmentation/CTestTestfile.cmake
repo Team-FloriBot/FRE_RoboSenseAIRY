c0e69fb0b929afffe4f6a423c8fea22026714a5e
FabioSchaaf <fabio.sch2af@gmail.comgit config --global user.email fabio.sch2af@gmail.com>
2026-06-09 22:58:07 +0200

    2D-topics as laserscan

---
+++ build/ground_segmentation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fabio/FRE_RoboSenseAIRY/ground_segmentation
+# Build directory: /home/fabio/FRE_RoboSenseAIRY/build/ground_segmentation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
