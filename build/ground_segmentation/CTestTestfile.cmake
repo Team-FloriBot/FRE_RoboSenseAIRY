8fb192e5519526651f4a9a862605c3823656388c
norikaschneider <norikaschneider@gmail.com>
2026-06-10 10:36:54 +0200

    deltedet build log and install

--- build/ground_segmentation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fabio/FRE_RoboSenseAIRY/ground_segmentation
-# Build directory: /home/fabio/FRE_RoboSenseAIRY/build/ground_segmentation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
